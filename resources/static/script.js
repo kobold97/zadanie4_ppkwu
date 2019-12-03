67dfd67fe24f780d89cb4f619421cd7db14b430c
kobold97 <kobold97@gmail.com>
2019-12-03 17:10:19 +0100

    functionality fix

--- resources/static/script.js
+++ resources/static/script.js
@@ -1,12 +1,17 @@
 function get(url) {
     return new Promise(function(resolve, reject) {
         var req = new XMLHttpRequest();
+		
+
         req.open('GET', url);
+		req.setRequestHeader('Access-Control-Allow-Origin', '*');
+		req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 
         req.onload = function() {
             if (req.status == 200) {
                 resolve(req.response);
             } else {
+				console.log(req);
                 reject(Error(req.statusText));
             }
         };
@@ -20,14 +25,18 @@ function get(url) {
 
 var url = "https://adm.edu.p.lodz.pl/user/users.php?search=";
 
-document.querySelectorAll('.search').addEventListener("click", getEmployees, false);
+var search_button = document.querySelectorAll('.search');
+search_button[0].addEventListener("click", getEmployees, false);
 
 function getEmployees() {
-    name = document.querySelectorAll('.name').text;
-    surname = document.querySelectorAll('.surname').text;
+    name = document.querySelectorAll('.name')[0].value;
+    surname = document.querySelectorAll('.surname')[0].value;
+
     request = url + name + "+" + surname;
-  
+  	console.log("url ", request);
+
     get(request).then(function(response) {
+		console.log("hejka: " + response);
         var list = document.createElement('ul');
         for (var i = 0; i < response.length; i++) {
             var item = document.createElement('li');
